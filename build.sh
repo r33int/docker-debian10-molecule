f3786287ba7b42b7425ca089ed1e1d2590783802
Enzo <me@r33.space>
2020-12-21 00:12:04 +0100

    Update repo url

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -t jgeusebroek/debian10-molecule .
+docker build --rm -t r33ints/debian10-molecule .
--- buildNoCache.sh
+++ buildNoCache.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm --pull --no-cache -t jgeusebroek/debian10-molecule .
+docker build --rm --pull --no-cache -t r33ints/debian10-molecule .
